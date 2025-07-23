b6320c597be67462cb268f2447d93ea00c609052
harshita050402 <harshitamaheshwari0504@gmail.com>
2025-07-23 13:34:37 +0530

    added workflow dispatch

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/cicd-pipeline/",
   server: {
     open: true,
-    port: 3001,
-  }
+    port: 3001
+  },
 })
